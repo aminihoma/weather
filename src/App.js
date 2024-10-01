49a7f3627ad7d457a8127ad505856e47569f408d
aminihoma <134835686+aminihoma@users.noreply.github.com>
2024-10-01 12:30:59 +0330

    add footer links

--- src/App.js
+++ src/App.js
@@ -11,6 +11,28 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Weather App</h1>
         <Weather city="Tehran" />
+        <p>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/homa-amini-ba327296"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Homa Amini
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/aminihoma/homework-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a href="#" target="_blank" rel="noreferrer">
+            hosted on Netlify
+          </a>
+        </p>
       </header>
     </div>
   );
